884c5e207e679a5b027cc33654e8370b7183b51a
Samuel Guo <guosamuel1114@gmail.com>
2019-02-28 05:21:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,6 +83,13 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
+  holiday_hash.each do |season, holidays|
+    holida.each do |occassion, supplies|
+      if supplies.include?("BBQ")
+        return occassion
+      end
+    end
+  end
 end
 
 
